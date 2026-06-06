fb821f76df2c48697b4df651d80db09f5351347e
farhan <2310631170017@student.unsika.ac.id>
2026-06-06 12:40:05 +0700

    fix api index and vercel json routing

--- api/index.php
+++ api/index.php
@@ -1,11 +1,4 @@
 <?php
 
-// 1. Mindahin folder compile views ke /tmp khusus buat Vercel biar gak Error 500
-$compiledViews = '/tmp/storage/framework/views';
-if (!is_dir($compiledViews)) {
-    mkdir($compiledViews, 0755, true);
-}
-putenv("VIEW_COMPILED_PATH={$compiledViews}");
-
-// 2. Eksekusi Laravel index yang asli
+// Mengarahkan ke file index.php bawaan folder public Laravel
 require __DIR__ . '/../public/index.php';
