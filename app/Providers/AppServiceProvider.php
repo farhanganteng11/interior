e6fd3117af5283b16e3ab1f293935b889242db37
farhan <2310631170017@student.unsika.ac.id>
2026-06-06 11:47:23 +0700

    Fix otomatis buat folder views di tmp

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,11 @@ class AppServiceProvider extends ServiceProvider
         // Supaya Laravel tidak eror read-only saat berjalan di Vercel
         if (isset($_SERVER['VERCEL_BACKGROUND_WORKER'])) {
             app()->useStoragePath('/tmp/storage');
+
+            // Paksa bikin folder views biar gak eror ->resolve('view')
+            if (!is_dir('/tmp/storage/framework/views')) {
+                mkdir('/tmp/storage/framework/views', 0755, true);
+            }
         }
     }
 
